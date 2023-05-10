fbfdd5336669b4d2bfe04b800f5fe7d3d9db5a0e
Yash-9274 <yash.tiwary20@st.niituniversity.in>
2023-05-10 11:47:39 +0530

    Read

--- blogfinal/src/components/StoryScreens/ReadListStoryItem.js
+++ blogfinal/src/components/StoryScreens/ReadListStoryItem.js
@@ -72,7 +72,7 @@ const ReadListStoryItem = ({story,editDate}) => {
 
             <section>
                 <div className="story-Image-Wrap">
-                    <img src={`/storyImages/${story.image}`} alt={story.title} width="180px" />
+                    <img src={`http://localhost:2000/storyImages/${story.image}`} alt={story.title} width="180px" />
                 </div>
 
             </section>

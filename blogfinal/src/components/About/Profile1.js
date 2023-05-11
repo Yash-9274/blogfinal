58858382caab2b3a13630c1602976e2cb01e88f6
Yash-9274 <yash.tiwary20@st.niituniversity.in>
2023-05-11 18:10:11 +0530

    Textc

--- blogfinal/src/components/About/Profile1.js
+++ blogfinal/src/components/About/Profile1.js
@@ -16,28 +16,28 @@ export const Profile1 = () => {
           <img src={YashDev} className="YashDev"></img>
           <div className="card-body1">
             <h2 className="Dev1Title">Yash Tiwary</h2>
-            <p>Currently studying at NIIT University under Data Science Specialisation.</p>
+            <p>Studying at NIIT University under Data Science Specialisation.</p>
           </div>
         </div>
         <div className="Developer2">
           <img src= {Ladali} className="LadaliDev"></img>
           <div className="card-body2">
             <h2 className="Dev2Title">Ladali Jain</h2>
-            <p>Click the button to watch on Jetflix app.</p>
+            <p>Studying at NIIT University under Artificial Intelligence Specialisation.</p>
           </div>
         </div>
         <div className="Developer3">
           <img src={EsheDev} className="EsheDev"></img>
           <div className="card-body3">
             <h2 className="Dev3Title">Eshe Dogra</h2>
-            <p>Studying Computer Science Engineering at NIIT University.</p>
+            <p>Studying at NIIT University under Data Science Specialisation.</p>
           </div>
         </div>
         <div className="Developer4">
           <img src={EsheDev} className="NitinDev"></img>
           <div className="card-body4">
             <h2 className="Dev4Title">Nitin Dhakad</h2>
-            <p>Software developer with experience in web development and mobile app development.</p>
+            <p>Studying at NIIT University under Data Science Specialisation.</p>
           </div>
           
           

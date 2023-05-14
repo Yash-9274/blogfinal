79f552807b8cf81d64a46628b0ac9f6a1ed7fa37
Eshe Dogra <eshe.dogra20@st.niituniversity.in>
2023-05-14 17:02:48 +0530

    chng2

--- blogfinal/src/components/About/Profile1.js
+++ blogfinal/src/components/About/Profile1.js
@@ -17,28 +17,33 @@ export const Profile1 = () => {
           <img src={YashDev} className="YashDev"></img>
           <div className="card-body1">
             <h2 className="Dev1Title">Yash Tiwary</h2>
-            <p>Studying at NIIT University under Data Science Specialisation.</p>
+            <p>Studying at NIIT University under Data Science Specialisation.</p><br></br>
+               <b>Email: Yash.tiwary20@st.niituniversity.in</b>
           </div>
         </div>
         <div className="Developer2">
           <img src= {LadaliDev} className="LadaliDev"></img>
           <div className="card-body2">
             <h2 className="Dev2Title">Ladali Jain</h2>
-            <p>Studying at NIIT University under Artificial Intelligence Specialisation.</p>
+            <p>Studying at NIIT University under Artificial Intelligence Specialisation.</p><br></br>
+               <b>Email: Ladali.jain20@st.niituniversity.in</b>
           </div>
         </div>
         <div className="Developer3">
           <img src={EsheDev} className="EsheDev"></img>
           <div className="card-body3">
             <h2 className="Dev3Title">Eshe Dogra</h2>
-            <p>Studying at NIIT University under Data Science Specialisation.</p>
+            <p>Studying at NIIT University under Data Science Specialisation.</p><br></br>
+               <b>Email: Eshe.dogra20@st.niituniversity.in</b>
           </div>
         </div>
         <div className="Developer4">
           <img src={NitinDev} className="NitinDev"></img>
           <div className="card-body4">
             <h2 className="Dev4Title">Nitin Dhakad</h2>
-            <p>Studying at NIIT University under Data Science Specialisation.</p>
+            <p>Studying at NIIT University under Artificial Intelligence.</p><br></br>
+               <b>Email: Nitin.dhakad20@st.niituniversity.in</b>
+            
           </div>
           
           
